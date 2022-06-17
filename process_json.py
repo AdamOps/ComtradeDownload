c7d822dcc093698d0c9ea3a7cfeee9d6eb5cca05
AdamOps <adamkuczynski@gmail.com>
2022-06-17 22:12:37 +0200

    Update

--- process_json.py
+++ process_json.py
@@ -4,7 +4,8 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join
 
-cd = "C:/Users/AdamKuczynski/OneDrive - SEO/Documenten/GEO Monitor/Data/"
+# cd = "C:/Users/AdamKuczynski/OneDrive - SEO/Documenten/GEO Monitor/Data/"
+cd = "C:/Users/adamk/OneDrive - SEO/Documenten/GEO Monitor/Data/"
 os.chdir(cd)
 file_list = [f for f in listdir(cd) if isfile(join(cd, f))]
 
@@ -13,7 +14,7 @@ for json_file in file_list:
         data_file = json.load(file)
         data_DF = pd.DataFrame(data_file['dataset'])
         without_extension = json_file.rstrip(".json")
-        export_name = cd + "csv_exports/" + without_extension + ".csv"
+        export_name = cd + "csv_exports_2019/" + without_extension + ".csv"
         # print(export_name)
         data_DF.to_csv(export_name, sep=";")
 
